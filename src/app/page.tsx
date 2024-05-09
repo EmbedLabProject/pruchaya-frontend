699251af7ac72a5f36541f0548fe0a872ec0ff67
ttontoey <65761088+ttontoey@users.noreply.github.com>
2024-05-09 23:26:25 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+  <main className="font-anuphan flex flex-col items-center w-full min-h-screen bg-gradient-to-b from-teal-500 to-emerald-200 pt-10 pb-24">
+    <h1 className="text-3xl font-semibold text-white mb-5">พืชญา</h1>
+    <p className=" text-white text-lg font-medium w-80">ปัญหา</p>
+    <div className="mt-1 w-80 h-32 bg-white rounded-lg opacity-60"></div>
+  </main>
+    
+    
+    </>
+  );
+}
---
+++ src/utils/fonts.ts
@@ -0,0 +1,7 @@
+import { Anuphan } from 'next/font/google';
+
+export const anu = Anuphan({
+    subsets: ['latin'],
+    variable: '--font-anuphan'
+});
+
