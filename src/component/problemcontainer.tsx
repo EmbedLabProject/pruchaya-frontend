53bc6bfe29bf7811b8aff4c37e98b48050b4c0ff
ttontoey <65761088+ttontoey@users.noreply.github.com>
2024-05-10 22:41:09 +0700

    feat: skeleton loading

--- src/component/problemcontainer.tsx
+++ src/component/problemcontainer.tsx
@@ -7,6 +7,8 @@ function ProblemContainer(props: any) {
     const {searchQuery, searchConstraint} = props;
 
     const [list, setList] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+
 
 
     const checkConstraint = function(desc: string) {
@@ -19,35 +21,44 @@ function ProblemContainer(props: any) {
 
     function checkMatchQuery(problem: any){
         if (problem.description.includes(searchQuery) && checkConstraint(problem.description)){
-            let desc = problem.description;
-            if (desc.length > 25) {
-                desc = desc.substring(0, 25) + "...";
-              }
-            return <ProblemBox key={problem.ticket_id} name={desc} dist="??"/>;
+            return true;
         }
-        else {
-            return null;
-        }
+        return false;
     }
 
 
     useEffect(() => {
         async function generateList(){
             const probs: any[] = await getAllProblems();
-            const temp: any[] = probs.map(p => checkMatchQuery(p));
+            const temp: any[] = [];
+            probs.forEach(p => {
+                if(checkMatchQuery(p)){
+                    let desc = p.description;
+                    if (desc.length > 25) {
+                    desc = desc.substring(0, 25) + "...";
+                    }
+                    temp.push(<ProblemBox key={p.ticket_id} name={desc} dist="??"/>);
+                }
+            });
             setList(temp);
+            setLoading(false);
         }
         generateList();
     },[searchQuery, searchConstraint.length]);
 
+    const notFoundUI = <div className="flex w-full items-center justify-center"><p className="text-xs font-light text-black">ไม่พบปัญหา</p></div>;
+    const skeletonUI = <><div className="flex flex-row w-full h-16 bg-white rounded-lg items-center justify-center p-2 gap-3 animate-pulse">
+        <p className="text-xs text-gray-400">กำลังโหลด...</p>
+        </div><div className="flex flex-row w-full h-16 bg-white rounded-lg items-center justify-center p-2 gap-3 animate-pulse">
+            <p className="text-xs text-gray-400">กำลังโหลด...</p>
+        </div></>;
     
     return (
         <>
             <div className="flex flex-row flex-wrap mt-1 w-80 h-fit max-h-56 overflow-y-scroll bg-white rounded-lg bg-opacity-60 p-3 gap-2">
                 {list}
-                {/* <div className="flex flex-row w-full h-16 bg-white rounded-lg items-center justify-center p-2 gap-3 animate-pulse">
-                    <p className="text-xs text-gray-400">กำลังโหลด...</p>
-                </div> */}
+                {(!loading && list.length == 0) ? notFoundUI : null}
+                {(loading) ? skeletonUI : null}
             </div>
         </>
 
