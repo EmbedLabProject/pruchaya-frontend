2294005a642cf9348c508c2b85c9fe7c2caeda71
Bo0K31 <ahtusaw@gmail.com>
2024-05-14 16:51:31 +0700

    sensorbox: all progress bar

--- src/component/tools/readsensor.tsx
+++ src/component/tools/readsensor.tsx
@@ -1,6 +1,10 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import { ArrowPathIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
+import { BeakerIcon } from "@heroicons/react/16/solid";
+import { SunIcon } from "@heroicons/react/16/solid";
+import { InformationCircleIcon } from "@heroicons/react/16/solid";
+import { ShieldExclamationIcon } from "@heroicons/react/16/solid";
 import SensorBox from "./sensorbox";
 function ReadSensor(props: any){
 
@@ -14,6 +18,7 @@ function ReadSensor(props: any){
     function reloadID(){
         setLoading(false)
     }
+
     const notFoundUI = <div className="flex w-full items-center justify-center"><p className="text-xs font-light text-black">พืชนี้ไม่มีในระบบ</p></div>;
     const skeletonUI = <><p className="flex flex-row w-full h-16 bg-white rounded-lg items-center justify-center p-2 gap-3 animate-pulse">
         <p className="text-xs text-gray-400">กำลังโหลด...</p>
@@ -29,6 +34,42 @@ function ReadSensor(props: any){
                 {(loading) ? skeletonUI : null}
             </h2>
         </article>
+        <div>
+                <div className="flex flex-row w-full h-16 items-center justify-start p-2 gap-1">
+                    <div className="w-6 h-6 rounded-full bg-gray-600 grid place-content-center">
+                        <BeakerIcon className="w-5 h-5"/>
+                    </div>
+                    <progress className=" w-64 rounded-lg bg-blue-800" value="90" max="100" id="waterMeter"></progress>
+                    <div className="flex flex-col items-start">
+                        <button className="">
+                            <InformationCircleIcon className="w-4 h-4 text-black"/>
+                        </button>
+                    </div>
+                </div>
+
+                <div className="flex flex-row w-full h-16 items-center justify-start p-2 gap-1">
+                    <div className="w-6 h-6 rounded-full bg-gray-600 grid place-content-center">
+                        <SunIcon className="w-5 h-5"/>
+                    </div>
+                    <progress className=" w-64 rounded-lg bg-blue-800" value="90" max="100" id="waterMeter"></progress>
+                    <div className="flex flex-col items-start">
+                        <button className="">
+                            <InformationCircleIcon className="w-4 h-4 text-black"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="flex flex-row w-full h-16 items-center justify-start p-2 gap-1">
+                    <div className="w-6 h-6 rounded-full bg-gray-600 grid place-content-center">
+                        <ShieldExclamationIcon className="w-5 h-5"/>
+                    </div>
+                    <progress className=" w-64 rounded-lg bg-blue-800" value="90" max="100" id="waterMeter"></progress>
+                    <div className="flex flex-col items-start">
+                        <button className="">
+                            <InformationCircleIcon className="w-4 h-4 text-black"/>
+                        </button>
+                    </div>
+                </div>
+            </div>
     </section>
 
     
@@ -37,7 +78,7 @@ function ReadSensor(props: any){
     return (<>
 
         <div className="w-80 h-35 flex flex-col bg-white rounded-lg opacity-60 mb-5 shadow-md">
-            <p className="text-xs text-black">Hi, I'm Sensor Reader!</p>
+            {/* <p className="text-xs text-black">Hi, I'm Sensor Reader!</p> */}
             <div className="flex flex-row w-80 mb-3 items-center justify-center gap-5">
                 <p className=" text-black text-xs w-35 mb-1">รหัสของอุปกรณ์</p>
                 <input value={searchQuery} onChange={event => setSearchQuery(event.target.value)} className="rounded-full shadow-md py-4 px-3 ps-10 w-45 h-4 text-xs text-black" type="text" placeholder="device ID"/>
@@ -51,9 +92,7 @@ function ReadSensor(props: any){
                     <ArrowPathIcon className="w-6 h-6"/>
                 </button>
             </div>
-            <div>
-                <SensorBox Icon ="water" percent={100}/>;
-            </div>
+            
 
             
         </div>
